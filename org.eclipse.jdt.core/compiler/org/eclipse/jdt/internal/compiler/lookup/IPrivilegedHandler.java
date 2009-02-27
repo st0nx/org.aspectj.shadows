edaedd60b5e1db411ae7b9e9ad441213e4f9e4ef
aclement <aclement>
2009-02-27 19:30:17 +0000

    266420: a way to determine if a privilegedhandler mentions a particular field

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/IPrivilegedHandler.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/IPrivilegedHandler.java
@@ -10,21 +10,24 @@
  ******************************************************************************/
 
 package org.eclipse.jdt.internal.compiler.lookup;
+
 //AspectJ Extension
 
 import org.eclipse.jdt.internal.compiler.ast.ASTNode;
 
-
 /**
- * This interface is used by SourceTypeBinding to provide a delegated lookup
- * instance. It is used to support AspectJ's inter-type declarations.
+ * This interface is used by SourceTypeBinding to provide a delegated lookup instance. It is used to support AspectJ's inter-type
+ * declarations.
  * 
  * These methods are equivalent to those of the same names and sigs in SourceTypeBinding.
  */
 public interface IPrivilegedHandler {
-	
+
 	FieldBinding getPrivilegedAccessField(FieldBinding baseField, ASTNode location);
-	
+
+	boolean definesPrivilegedAccessToField(FieldBinding field);
+
 	MethodBinding getPrivilegedAccessMethod(MethodBinding baseMethod, ASTNode location);
+
 	void notePrivilegedTypeAccess(ReferenceBinding type, ASTNode location);
 }
